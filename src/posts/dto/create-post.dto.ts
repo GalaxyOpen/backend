5e620a540420fef3ad8726a8935251aa9d76c9a9
GalxyOpen <keehong11@naver.com>
2026-02-12 14:49:00 +0900

    2026.02.12 dto 생성

---
+++ src/posts/dto/create-post.dto.ts
@@ -0,0 +1,12 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  @MaxLength(120)
+  title!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  content!: string;
+}
